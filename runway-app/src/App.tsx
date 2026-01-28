ad002c0e1afff62c1286904cf517c97b308dc3a3
alex <70367958+lolalex1@users.noreply.github.com>
2026-01-28 19:02:41 +1100

    feat: add react-icons dependency and implement icons in App component; update background style in index.css; create Sidebar component

--- runway-app/src/App.tsx
+++ runway-app/src/App.tsx
@@ -4,12 +4,18 @@ import './App.css'
 // Pages
 import CalendarPage from './pages/calendar'
 
+// Icons
+import { HiCalendarDateRange } from "react-icons/hi2";
+import { BsPeopleFill } from "react-icons/bs";
+import { FaHouseChimneyUser } from "react-icons/fa6";
+import { RiSettingsFill } from "react-icons/ri";
+
 function App() {
 	const [page, setPage] = useState('calendar')
 
 	return (
 		<>
-			<div className="w-screen h-screen bg-bg pt-24 flex flex-col items-center">
+			<div className="w-screen h-screen pt-12 flex flex-col items-center">
 				<div className="w-3/4 h-fit p-4 rounded-lg bg-surface-elevated flex flex-row justify-between items-center">
 					<div className="flex flex-col gap-2">
 						<span className="text-white font-bold text-2xl">Welcome back, Alex!</span>
@@ -19,14 +25,26 @@ function App() {
 				</div>
 				<div className="w-3/4 h-full flex flex-row pt-5">
 					<div className="w-1/5 h-full flex flex-col gap-1 border-r-2 border-surface">
-						<div className='ml-8 text-lg gap-4 flex flex-col text-gray-400 cursor-pointer h-fit'>
-							<span className={`transition-all duration-200 ${page === 'calendar' && 'text-white text-2xl'}`} onClick={() => setPage('calendar')}>Calendar</span>
-							<span className={`transition-all duration-200 ${page === 'people' && 'text-white text-2xl'}`} onClick={() => setPage('people')}>People</span>
-							<span className={`transition-all duration-200 ${page === 'shared' && 'text-white text-2xl'}`} onClick={() => setPage('shared')}>Shared</span>
-							<span className={`transition-all duration-200 ${page === 'settings' && 'text-white text-2xl'}`} onClick={() => setPage('settings')}>Settings</span>
+						<div className='text-lg gap-3 flex flex-col text-gray-400 cursor-pointer h-fit pr-4'>
+							<div className={`flex flex-row gap-3 items-center transition-all duration-200 pl-4 ${page === 'calendar' && 'bg-white/70 text-gray-900 font-semibold py-2 rounded-full scale-105'} ${page !== 'calendar' && 'hover:bg-neutral-800/60 hover:scale-105 hover:text-white rounded-full py-2'}`} onClick={() => setPage('calendar')}>
+								<HiCalendarDateRange className="text-lg" />
+								<span className="text-sm">Calendar</span>
+							</div>
+							<div className={`flex flex-row gap-3 items-center transition-all duration-200 pl-4 ${page === 'people' && 'bg-white/70 text-gray-900 font-semibold py-2 rounded-full scale-105'} ${page !== 'people' && 'hover:bg-neutral-800/60 hover:scale-105 hover:text-white rounded-full py-2	'}`} onClick={() => setPage('people')}>
+								<BsPeopleFill className="text-lg" />
+								<span className="text-sm">People</span>
+							</div>
+							<div className={`flex flex-row gap-3 items-center transition-all duration-200 pl-4 ${page === 'shared' && 'bg-white/70 text-gray-900 font-semibold py-2 rounded-full scale-105'} ${page !== 'shared' && 'hover:bg-neutral-800/60 hover:scale-105 hover:text-white rounded-full py-2'}`} onClick={() => setPage('shared')}>
+								<FaHouseChimneyUser className="text-lg" />
+								<span className="text-sm">Shared</span>
+							</div>
+							<div className={`flex flex-row gap-3 items-center transition-all duration-200 pl-4 ${page === 'settings' && 'bg-white/70 text-gray-900 font-semibold py-2 rounded-full scale-105'} ${page !== 'settings' && 'hover:bg-neutral-800/60 hover:scale-105 hover:text-white rounded-full py-2'}`} onClick={() => setPage('settings')}>
+								<RiSettingsFill className="text-lg" />
+								<span className="text-sm">Settings</span>
+							</div>
 						</div>
 					</div>
-					<div className="w-full h-fit px-4">
+					<div className="w-full h-fit pl-4">
 						{page === 'calendar' && <CalendarPage />}
 						{page === 'people' && <span className="text-white text-3xl font-bold">People Page</span>}
 						{page === 'shared' && <span className="text-white text-3xl font-bold">Shared Page</span>}
